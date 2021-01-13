b44f9b7b61da217b10814ce7cc3d6560cab620b6
João Paulo <jpncavalcante@gmail.com>
2021-01-13 16:58:20 -0300

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='BR_ML',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/nogueira-cavalcante/BR_ML.git',
+    author='João Paulo Nogueira Cavalcante',
+    author_email='jpncavalcante@gmail.com',
+    license='unlicense',
+    packages=['BR_ML'],
+    zip_safe=False
+)
+
